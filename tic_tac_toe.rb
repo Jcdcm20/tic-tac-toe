2fd219c2dc14252204fa5eb337685673164f5fb5
Jcdcm20 <jcdcm20@gmail.com>
2023-02-27 10:38:43 -0300

    Make clases and look clearer

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,41 +1,29 @@
 class Board
+  attr_accessor :board
+
   def initialize
     @board = [' ', ' ', ' ',
               ' ', ' ', ' ',
               ' ', ' ', ' ']
-  end
-
-  def draw_board
-    system 'clear'
-    puts 'TIC TAC TOE'
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
-    puts '-----------'
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
-    puts '-----------'
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
-  end
-
-  def update_board(square, player)
-    @board[square - 1] = player.symbol
-  end
-
-  def check_winner(player)
-    if player.winner == true
-      puts "#{player.symbol} is the winner!"
-      return true
     end
-  end
 
-  def check_square(square)
-    if @board[square - 1] == ' '
-      return true
+    def draw_board
+      system 'clear'
+      puts 'TIC TAC TOE'
+      puts " #{board[0]} | #{board[1]} | #{board[2] }"
+      puts '-----------'
+      puts " #{board[3]} | #{board[4]} | #{board[5]} "
+      puts '-----------'
+      puts " #{board[6]} | #{board[7]} | #{board[8]} "
     end
-      return false
-  end
+
+    def check_square(square)
+      board[square] == ' ' ? true : false
+    end
 end
 
 class Player
-  attr_reader :symbol
+  attr_reader :symbol, :square
   attr_accessor :winner
 
   def initialize(symbol)
@@ -43,37 +31,35 @@ class Player
     @winner = false
   end
 
-  def square
-    gets.to_i
+  def get_square
+    @square = gets.to_i - 1
+  end
+
+  def mark_board(board)
+    board.board[square] = symbol
   end
 end
 
 board = Board.new
+board.draw_board
+
 x = Player.new('X')
 o = Player.new('O')
 
-players = [x, o]
-winner = false
 
-player = players.sample
 
-while !board.check_winner(player) do
-  board.draw_board
+player = x
 
-  puts "#{player.symbol} - Choose 1 - 9: "
-  square = player.square
+
+i = 0
+while i < 5
+  print "#{player.symbol} choose 1 - 9: "
   
-  if board.check_square(square)
-    board.update_board(square, player)
-  else
-    next
-  end
+  board.check_square(player.get_square) ? player.mark_board(board) : next
 
   board.draw_board
 
-  if player == players[0]
-    player = players[1]
-  else
-    player = players[0]
-  end
+  # Switch player
+  player == x ? player = o : player = x
+  i += 1
 end
