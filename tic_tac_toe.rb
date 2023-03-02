e46e3342cbc599332216d20c14a0d826b07a9591
Jcdcm20 <jcdcm20@gmail.com>
2023-03-02 15:19:07 -0300

    Add endgames

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,14 +8,14 @@ class Board
               ' ', ' ', ' ']
 
     @winning_conditions = [
-                          [board[0], board[1], board[2]],
-                          [board[3], board[4], board[5]],
-                          [board[6], board[7], board[8]],
-                          [board[0], board[3], board[6]],
-                          [board[1], board[4], board[7]],
-                          [board[2], board[5], board[8]],
-                          [board[0], board[4], board[8]],
-                          [board[2], board[4], board[6]]
+                          [0, 1, 2],
+                          [3, 4, 5],
+                          [6, 7, 8],
+                          [0, 3, 6],
+                          [1, 4, 7],
+                          [2, 5, 8],
+                          [0, 4, 8],
+                          [2, 4, 6]
     ]
   end
 
@@ -33,10 +33,12 @@ class Board
     board[square] == ' ' ? true : false
   end
 
-  def check_winner(player)
-    if winning_conditions.any? { |condition| condition.all? { |square| square == player.symbol}}
-      return true
-    end
+  def check_win(symbol)
+    winning_conditions.any? { |condition| condition.all? { |index| board[index] == symbol } } ? true : false
+  end
+
+  def check_draw()
+    board.all? { |square| square != ' ' } ? true : false
   end
 end
 
@@ -68,15 +70,22 @@ o = Player.new('O')
 
 player = x
 
-game = true
+endgame = false
 
-while game
+until endgame
   print "#{player.symbol} choose 1 - 9: "
   
   board.check_square(player.get_square) ? player.mark_board(board) : next
 
   board.draw_board
-
+  # Check for win
+  if board.check_win(player.symbol)
+    puts "#{player.symbol} has won!!!!"
+    endgame = true    
+  elsif board.check_draw()
+    puts "The game ends in a draw!"
+    endgame = true
+  end
   # Switch player
   player == x ? player = o : player = x
   
